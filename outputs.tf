a1763580047f70de466ad4c3d18b7e4c069a99e4
z.fu177 <z.fu177@example.com>
2023-11-30 17:46:43 -0700

    add output distribution id

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
 output "bucket_name" {
   value = aws_s3_bucket.website.id
 }
